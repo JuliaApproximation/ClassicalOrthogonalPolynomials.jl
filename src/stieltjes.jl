bd72a9536215d8aadb7f3b09fec7b911b1f89364
Sheehan Olver <solver@mac.com>
2021-03-19 14:59:12 +0000

    Start associated

--- src/stieltjes.jl
+++ src/stieltjes.jl
@@ -1,3 +1,49 @@
+####
+# Associated
+####
+
+"""
+    Associated(P)
+
+constructs the associated orthogonal polynomials for P, which have the Jacobi matrix
+
+    jacobimatrix(P)[2:end,2:end]
+
+and constant first term. Or alternatively
+
+    w = orthogonalityweight(P)
+    A = recurrencecoefficients(P)[1]
+    Associated(P) == (w/(sum(w)*A[1]))'*((P[:,2:end]' - P[:,2:end]) ./ (x' - x))
+
+where `x = axes(P,1)`.
+"""
+
+struct Associated{T, OPs<:AbstractQuasiMatrix{T}} <: OrthogonalPolynomial{T}
+    P::OPs
+end
+
+Associated(Q::Associated) = Q
+associated(P) = Associated(P)
+
+axes(Q::Associated) = axes(Q.P)
+==(A::Associated, B::Associated) = A.P == B.P
+
+
+function associated_jacobimatrix(X::Tridiagonal)
+    c,a,b = subdiagonaldata(X),diagonaldata(X),supdiagonaldata(X)
+    Tridiagonal(c[2:end], a[2:end], b[2:end])
+end
+
+function associated_jacobimatrix(X::SymTridiagonal)
+    a,b = diagonaldata(X),supdiagonaldata(X)
+    SymTridiagonal(a[2:end], b[2:end])
+end
+jacobimatrix(a::Associated) = associated_jacobimatrix(jacobimatrix(a.P))
+
+associated(::ChebyshevT{T}) where T = ChebyshevU{T}()
+associated(::ChebyshevU{T}) where T = ChebyshevU{T}()
+
+
 const StieltjesPoint{T,V,D} = BroadcastQuasiMatrix{T,typeof(inv),Tuple{BroadcastQuasiMatrix{T,typeof(-),Tuple{T,QuasiAdjoint{V,Inclusion{V,D}}}}}}
 const ConvKernel{T,D} = BroadcastQuasiMatrix{T,typeof(-),Tuple{D,QuasiAdjoint{T,D}}}
 const Hilbert{T,D} = BroadcastQuasiMatrix{T,typeof(inv),Tuple{ConvKernel{T,Inclusion{T,D}}}}
@@ -5,44 +51,57 @@ const LogKernel{T,D} = BroadcastQuasiMatrix{T,typeof(log),Tuple{BroadcastQuasiMa
 const PowKernel{T,D,F<:Real} = BroadcastQuasiMatrix{T,typeof(^),Tuple{BroadcastQuasiMatrix{T,typeof(abs),Tuple{ConvKernel{T,Inclusion{T,D}}}},F}}
 
 
-@simplify function *(S::StieltjesPoint{<:Any,<:Any,<:ChebyshevInterval}, wT::WeightedBasis{<:Any,<:ChebyshevTWeight,<:ChebyshevT})
-    w,T = wT.args
-    J = jacobimatrix(T)
-    z, x = parent(S).args[1].args
-    transpose((J'-z*I) \ [-π; zeros(∞)])
+@simplify function *(H::Hilbert, w::ChebyshevTWeight)
+    T = promote_type(eltype(H), eltype(w))
+    zeros(T, axes(w,1))
 end
 
-@simplify function *(H::Hilbert{<:Any,<:ChebyshevInterval}, wT::WeightedBasis{<:Any,<:ChebyshevTWeight,<:ChebyshevT}) 
+@simplify function *(H::Hilbert, w::ChebyshevUWeight)
+    T = promote_type(eltype(H), eltype(w))
+    fill(convert(T,π), axes(w,1))
+end
+
+@simplify function *(H::Hilbert, wT::Weighted{<:Any,<:ChebyshevT}) 
     T = promote_type(eltype(H), eltype(wT))
-    ApplyQuasiArray(*, ChebyshevU{T}(), _BandedMatrix(Fill(-convert(T,π),1,∞), ℵ₀, -1, 1))
+    ChebyshevU{T}() * _BandedMatrix(Fill(-convert(T,π),1,∞), ℵ₀, -1, 1)
 end
 
-@simplify function *(H::Hilbert{<:Any,<:ChebyshevInterval}, wU::WeightedBasis{<:Any,<:ChebyshevUWeight,<:ChebyshevU}) 
+@simplify function *(H::Hilbert, wU::Weighted{<:Any,<:ChebyshevU}) 
     T = promote_type(eltype(H), eltype(wU))
-    ApplyQuasiArray(*, ChebyshevT{T}(), _BandedMatrix(Fill(convert(T,π),1,∞), ℵ₀, 1, -1))
+    ChebyshevT{T}() * _BandedMatrix(Fill(convert(T,π),1,∞), ℵ₀, 1, -1)
+end
+
+
+@simplify function *(H::Hilbert, wP::Weighted{<:Any,<:OrthogonalPolynomial}) 
+    P = wP.P
+    w = orthogonalityweight(P)
+    A = recurrencecoefficients(P)
+    (A[1]*sum(w))*associated(P) + (H*w) .* P
 end
 
 ### 
 # LogKernel
 ###
 
-@simplify function *(L::LogKernel{<:Any,<:ChebyshevInterval}, wT::WeightedBasis{<:Any,<:ChebyshevTWeight,<:ChebyshevT}) 
+@simplify function *(L::LogKernel, wT::Weighted{<:Any,<:ChebyshevT}) 
     T = promote_type(eltype(L), eltype(wT))
-    ApplyQuasiArray(*, ChebyshevT{T}(), Diagonal(Vcat(-π*log(2*one(T)),-convert(T,π)./(1:∞))))
+    ChebyshevT{T}() * Diagonal(Vcat(-π*log(2*one(T)),-convert(T,π)./(1:∞)))
 end
+
 
 
 ### 
 # PowKernel
 ###
 
-@simplify function *(K::PowKernel{<:Any,<:ChebyshevInterval}, wT::WeightedBasis{<:Any,<:JacobiWeight,<:Jacobi}) 
+@simplify function *(K::PowKernel, wT::Weighted{<:Any,<:Jacobi}) 
     T = promote_type(eltype(K), eltype(wT))
     cnv,α = K.args
     x,y = K.args[1].args[1].args
     @assert x' == y
     β = (-α-1)/2
-    ApplyQuasiArray(*, ChebyshevT{T}(), Diagonal(1:∞))
+    error("Not implemented")
+    # ChebyshevT{T}() * Diagonal(1:∞)
 end
 
 
@@ -50,7 +109,21 @@ end
 # StieltjesPoint
 ####
 
-@simplify function *(S::StieltjesPoint, wT::SubQuasiArray{<:Any,2,<:WeightedBasis,<:Tuple{<:AbstractAffineQuasiVector,<:Any}})
+@simplify function *(S::StieltjesPoint, w::AbstractJacobiWeight)
+    α,β = w.a,w.b
+    (x = 2/(1-z);normalization(n,α,β)*HypergeometricFunctions.mxa_₂F₁(n+1,n+α+1,2n+α+β+2,x))
+end
+
+@simplify function *(S::StieltjesPoint, wP::Weighted)
+    P = wP.P
+    w = orthogonalityweight(P)
+    X = jacobimatrix(P)
+    z, x = parent(S).args[1].args
+    transpose((X'-z*I) \ [-sum(w)*_p0(P); zeros(∞)])
+end
+
+
+@simplify function *(S::StieltjesPoint, wT::SubQuasiArray{<:Any,2,<:Any,<:Tuple{<:AbstractAffineQuasiVector,<:Any}})
     P = parent(wT)
     z, x = parent(S).args[1].args
     z̃ = inbounds_getindex(parentindices(wT)[1], z)
@@ -58,21 +131,33 @@ end
     (inv.(z̃ .- x̃') * P)[:,parentindices(wT)[2]]
 end
 
-@simplify function *(H::Hilbert, wT::SubQuasiArray{<:Any,2,<:WeightedBasis,<:Tuple{<:AbstractAffineQuasiVector,<:Any}}) 
+@simplify function *(H::Hilbert, wT::SubQuasiArray{<:Any,2,<:Any,<:Tuple{<:AbstractAffineQuasiVector,<:Any}}) 
     P = parent(wT)
     x = axes(P,1)
     apply(*, inv.(x .- x'), P)[parentindices(wT)...]
 end
 
 
-@simplify function *(L::LogKernel, wT::SubQuasiArray{<:Any,2,<:WeightedBasis,<:Tuple{<:AbstractAffineQuasiVector,<:Slice}}) 
+@simplify function *(L::LogKernel, wT::SubQuasiArray{<:Any,2,<:Any,<:Tuple{<:AbstractAffineQuasiVector,<:Slice}}) 
     V = promote_type(eltype(L), eltype(wT))
-    P = parent(wT)
+    wP = parent(wT)
     kr, jr = parentindices(wT)
-    @assert P isa WeightedBasis{<:Any,<:ChebyshevWeight,<:Chebyshev}
-    x = axes(P,1)
-    w,T = P.args
-    D = T \ apply(*, log.(abs.(x .- x')), P)
+    x = axes(wP,1)
+    w,T = arguments(ApplyLayout{typeof(*)}(), wP)
+    @assert w isa ChebyshevTWeight
+    @assert T isa ChebyshevT
+    D = T \ (log.(abs.(x .- x')) * wP)
     c = inv(2*kr.A)
     T[kr,:] * Diagonal(Vcat(2*convert(V,π)*c*log(c), 2c*D.diag.args[2]))
+end
+
+
+
+### generic fallback
+for Op in (:Hilbert, :StieltjesPoint, :LogKernel, :PowKernel)
+    @eval @simplify function *(H::$Op, wP::WeightedBasis{<:Any,<:Weight,<:Any}) 
+        w,P = wP.args
+        Q = OrthogonalPolynomial(w)
+        (H * Weighted(Q)) * (Q \ P)
+    end
 end
--- test/test_stieltjes.jl
+++ test/test_stieltjes.jl
@@ -1,71 +1,83 @@
 using ClassicalOrthogonalPolynomials, ContinuumArrays, Test
-import ClassicalOrthogonalPolynomials: Hilbert, StieltjesPoint, ChebyshevInterval
-@testset "Stieltjes" begin
-    T = Chebyshev()
-    wT = ChebyshevWeight() .* T
-    x = axes(wT,1)
-    z = 0.1+0.2im
-    S = inv.(z .- x')
-    @test S isa StieltjesPoint{ComplexF64,Float64,ChebyshevInterval{Float64}}
-    f = wT * [[1,2,3]; zeros(∞)];
-    J = T \ (x .* T)
-    # TODO: fix LazyBandedMatrices.copy(M::Mul{Broadcast...}) to call simplify
-    @test_broken π*((z*I-J) \ f.args[2])[1,1] ≈ (S*f)[1]
-    @test π*((z*I-J) \ f.args[2])[1,1] ≈ (S*f.args[1]*f.args[2])[1]
+import ClassicalOrthogonalPolynomials: Hilbert, StieltjesPoint, ChebyshevInterval, associated, Associated
 
-    x = Inclusion(0..1)
-    y = 2x .- 1
-    wT2 = wT[y,:]
-    S = inv.(z .- x')
-    f = wT2 * [[1,2,3]; zeros(∞)];
-
-    @test (π/2*(((z-1/2)*I - J/2) \ f.args[2]))[1] ≈ (S*f.args[1]*f.args[2])[1]
+@testset "Associated" begin
+    T = ChebyshevT()
+    U = ChebyshevU()
+    @test associated(T) ≡ U
+    @test associated(U) ≡ U
+    @test Associated(T)[0.1,1:10] == Associated(U)[0.1,1:10] == U[0.1,1:10]
 end
 
-@testset "Hilbert" begin
-    wT = ChebyshevTWeight() .* ChebyshevT()
-    wU = ChebyshevUWeight() .* ChebyshevU()
-    x = axes(wT,1)
-    H = inv.(x .- x')
-    @test H isa Hilbert{Float64,ChebyshevInterval{Float64}}
 
-    @test (Ultraspherical(1) \ (H*wT))[1:10,1:10] == diagm(1 => fill(-π,9))
-    @test (Chebyshev() \ (H*wU))[1:10,1:10] == diagm(-1 => fill(1.0π,9))
+@testset "Singular integrals" begin
+    @testset "Stieltjes" begin
+        T = Chebyshev()
+        wT = ChebyshevWeight() .* T
+        x = axes(wT,1)
+        z = 0.1+0.2im
+        S = inv.(z .- x')
+        @test S isa StieltjesPoint{ComplexF64,Float64,ChebyshevInterval{Float64}}
+        f = wT * [[1,2,3]; zeros(∞)];
+        J = T \ (x .* T)
+        # TODO: fix LazyBandedMatrices.copy(M::Mul{Broadcast...}) to call simplify
+        @test_broken π*((z*I-J) \ f.args[2])[1,1] ≈ (S*f)[1]
+        @test π*((z*I-J) \ f.args[2])[1,1] ≈ (S*f.args[1]*f.args[2])[1]
 
-    # check consistency
-    @test (Ultraspherical(1) \ (H*wT) * (wT \ wU))[1:10,1:10] == 
-                ((Ultraspherical(1) \ Chebyshev()) * (Chebyshev() \ (H*wU)))[1:10,1:10]
+        x = Inclusion(0..1)
+        y = 2x .- 1
+        wT2 = wT[y,:]
+        S = inv.(z .- x')
+        f = wT2 * [[1,2,3]; zeros(∞)];
 
-    # Other axes
-    x = Inclusion(0..1)
-    y = 2x .- 1
-    H = inv.(x .- x')
+        @test (π/2*(((z-1/2)*I - J/2) \ f.args[2]))[1] ≈ (S*f.args[1]*f.args[2])[1]
+    end
 
-    wT2 = wT[y,:]
-    wU2 = wU[y,:]
-    @test (Ultraspherical(1)[y,:]\(H*wT2))[1:10,1:10] == diagm(1 => fill(-π,9))
-    @test_broken (Chebyshev()[y,:]\(H*wU2))[1:10,1:10] == diagm(-1 => fill(1.0π,9))
-end
+    @testset "Hilbert" begin
+        wT = ChebyshevTWeight() .* ChebyshevT()
+        wU = ChebyshevUWeight() .* ChebyshevU()
+        x = axes(wT,1)
+        H = inv.(x .- x')
+        @test H isa Hilbert{Float64,ChebyshevInterval{Float64}}
 
-@testset "Log kernel" begin
-    T = Chebyshev()
-    wT = ChebyshevWeight() .* Chebyshev()
-    x = axes(wT,1)
-    L = log.(abs.(x .- x'))
-    D = T \ (L * wT)
-    @test ((L * wT) * (T \ exp.(x)))[0.] ≈ -2.3347795490945797  # Mathematica
+        @test (Ultraspherical(1) \ (H*wT))[1:10,1:10] == diagm(1 => fill(-π,9))
+        @test (Chebyshev() \ (H*wU))[1:10,1:10] == diagm(-1 => fill(1.0π,9))
 
-    x = Inclusion(-1..1)
-    T = Chebyshev()[1x, :]
-    L = log.(abs.(x .- x'))
-    wT = (ChebyshevWeight() .* Chebyshev())[1x, :]
-    @test (T \ (L*wT))[1:10,1:10] ≈ D[1:10,1:10]
+        # check consistency
+        @test (Ultraspherical(1) \ (H*wT) * (wT \ wU))[1:10,1:10] == 
+                    ((Ultraspherical(1) \ Chebyshev()) * (Chebyshev() \ (H*wU)))[1:10,1:10]
 
-    x = Inclusion(0..1)
-    T = Chebyshev()[2x.-1, :]
-    wT = (ChebyshevWeight() .* Chebyshev())[2x .- 1, :]
-    L = log.(abs.(x .- x'))
-    u =  wT * (2 *(T \ exp.(x)))
-    @test u[0.1] ≈ exp(0.1)/sqrt(0.1-0.1^2)
-    @test_broken (L * u)[0.5] ≈ -7.471469928754152 # Mathematica
+        # Other axes
+        x = Inclusion(0..1)
+        y = 2x .- 1
+        H = inv.(x .- x')
+
+        wT2 = wT[y,:]
+        wU2 = wU[y,:]
+        @test (Ultraspherical(1)[y,:]\(H*wT2))[1:10,1:10] == diagm(1 => fill(-π,9))
+        @test_broken (Chebyshev()[y,:]\(H*wU2))[1:10,1:10] == diagm(-1 => fill(1.0π,9))
+    end
+
+    @testset "Log kernel" begin
+        T = Chebyshev()
+        wT = ChebyshevWeight() .* Chebyshev()
+        x = axes(wT,1)
+        L = log.(abs.(x .- x'))
+        D = T \ (L * wT)
+        @test ((L * wT) * (T \ exp.(x)))[0.] ≈ -2.3347795490945797  # Mathematica
+
+        x = Inclusion(-1..1)
+        T = Chebyshev()[1x, :]
+        L = log.(abs.(x .- x'))
+        wT = (ChebyshevWeight() .* Chebyshev())[1x, :]
+        @test (T \ (L*wT))[1:10,1:10] ≈ D[1:10,1:10]
+
+        x = Inclusion(0..1)
+        T = Chebyshev()[2x.-1, :]
+        wT = (ChebyshevWeight() .* Chebyshev())[2x .- 1, :]
+        L = log.(abs.(x .- x'))
+        u =  wT * (2 *(T \ exp.(x)))
+        @test u[0.1] ≈ exp(0.1)/sqrt(0.1-0.1^2)
+        @test_broken (L * u)[0.5] ≈ -7.471469928754152 # Mathematica
+    end
 end
