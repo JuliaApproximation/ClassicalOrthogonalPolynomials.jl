a69f8431e2cef32c8bb93ca22ee250841b14941b
Timon Salar Gutleb <timon.gutleb@gmail.com>
2021-08-13 16:02:22 +0100

    clean up tests

--- test/test_decompOPs.jl
+++ test/test_decompOPs.jl
@@ -40,199 +40,193 @@ import LazyBandedMatrices: SymTridiagonal
     end
 end
 
-@testset "Comparison with Lanczos and Classical, non-Clenshaw" begin
-    @testset "w(x) = x^2*(1-x)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        w = (Jx^2 - Jx^3)
-        # compute Jacobi matrix via cholesky
-        Jchol = cholesky_jacobimatrix(w)
-        # compute Jacobi matrix via classical recurrence
-        Q = Normalized(Jacobi(1,2)[affine(0..1,Inclusion(-1..1)),:])
-        Jclass = jacobimatrix(Q)
-        # compute Jacobi matrix via Lanczos
-        wf = x.^2 .* (1 .- x)
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison with Lanczos
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-        # Comparison with Classical
-        @test Jchol[1:500,1:500] ≈ Jclass[1:500,1:500]
+@testset "Comparison with Lanczos and Classical" begin
+    @testset "Not using Clenshaw" begin
+        @testset "w(x) = x^2*(1-x)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            Jx = symmjacobim(J)
+            w = (Jx^2 - Jx^3)
+            # compute Jacobi matrix via cholesky
+            Jchol = cholesky_jacobimatrix(w)
+            # compute Jacobi matrix via classical recurrence
+            Q = Normalized(Jacobi(1,2)[affine(0..1,Inclusion(-1..1)),:])
+            Jclass = jacobimatrix(Q)
+            # compute Jacobi matrix via Lanczos
+            wf = x.^2 .* (1 .- x)
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison with Lanczos
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+            # Comparison with Classical
+            @test Jchol[1:500,1:500] ≈ Jclass[1:500,1:500]
+        end
+
+        @testset "w(x) = (1-x^2)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            Jx = symmjacobim(J)
+            w = (I - Jx^2)
+            # compute Jacobi matrix via cholesky
+            Jchol = cholesky_jacobimatrix(w)
+            # compute Jacobi matrix via Lanczos
+            wf = (1 .- x.^2)
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
+
+        @testset "w(x) = (1-x^4)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            Jx = symmjacobim(J)
+            w = (I - Jx^4)
+            # compute Jacobi matrix via cholesky
+            Jchol = cholesky_jacobimatrix(w)
+            # compute Jacobi matrix via Lanczos
+            wf = (1 .- x.^4)
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
+
+        @testset "w(x) = (1.014-x^3)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            Jx = symmjacobim(J)
+            t = 1.014
+            w = (t*I - Jx^3)
+            # compute Jacobi matrix via cholesky
+            Jchol = cholesky_jacobimatrix(w)
+            # compute Jacobi matrix via Lanczos
+            wf = (t .- x.^3)
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
     end
 
-    @testset "w(x) = (1-x^2)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        w = (I - Jx^2)
-        # compute Jacobi matrix via cholesky
-        Jchol = cholesky_jacobimatrix(w)
-        # compute Jacobi matrix via Lanczos
-        wf = (1 .- x.^2)
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+    @testset "Using Clenshaw for polynomial weights" begin
+        @testset "w(x) = x^2*(1-x)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            wf(x) = x^2*(1-x)
+            # compute Jacobi matrix via cholesky
+            W = P \ (wf.(x) .* P)
+            Jchol = cholesky_jacobimatrix(Symmetric(W))
+            # compute Jacobi matrix via classical recurrence
+            Q = Normalized(Jacobi(1,2)[affine(0..1,Inclusion(-1..1)),:])
+            Jclass = jacobimatrix(Q)
+            # compute Jacobi matrix via Lanczos
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison with Lanczos
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+            # Comparison with Classical
+            @test Jchol[1:500,1:500] ≈ Jclass[1:500,1:500]
+        end
+
+        @testset "w(x) = (1-x^2)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            wf(x) = (1-x^2)
+            # compute Jacobi matrix via cholesky
+            W = P \ (wf.(x) .* P)
+            Jchol = cholesky_jacobimatrix(Symmetric(W))
+            # compute Jacobi matrix via Lanczos
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison with Lanczos
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
+
+        @testset "w(x) = (1-x^4)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            wf(x) = (1-x^4)
+            # compute Jacobi matrix via cholesky
+            W = P \ (wf.(x) .* P)
+            Jchol = cholesky_jacobimatrix(Symmetric(W))
+            # compute Jacobi matrix via Lanczos
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison with Lanczos
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
+
+        @testset "w(x) = (1.014-x^3)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            wf(x) = 1.014-x^4
+            # compute Jacobi matrix via cholesky
+            W = P \ (wf.(x) .* P)
+            Jchol = cholesky_jacobimatrix(Symmetric(W))
+            # compute Jacobi matrix via Lanczos
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison with Lanczos
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
     end
 
-    @testset "w(x) = (1-x^4)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        w = (I - Jx^4)
-        # compute Jacobi matrix via cholesky
-        Jchol = cholesky_jacobimatrix(w)
-        # compute Jacobi matrix via Lanczos
-        wf = (1 .- x.^4)
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-    end
-
-    @testset "w(x) = (1.014-x^3)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        t = 1.014
-        w = (t*I - Jx^3)
-        # compute Jacobi matrix via cholesky
-        Jchol = cholesky_jacobimatrix(w)
-        # compute Jacobi matrix via Lanczos
-        wf = (t .- x.^3)
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-    end
-end
-
-@testset "Comparison with Lanczos and Classical, with Clenshaw, polynomial weights" begin
-    @testset "w(x) = x^2*(1-x)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        wf(x) = x^2*(1-x)
-        # compute Jacobi matrix via cholesky
-        W = P \ (wf.(x) .* P)
-        Jchol = cholesky_jacobimatrix(Symmetric(W))
-        # compute Jacobi matrix via classical recurrence
-        Q = Normalized(Jacobi(1,2)[affine(0..1,Inclusion(-1..1)),:])
-        Jclass = jacobimatrix(Q)
-        # compute Jacobi matrix via Lanczos
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison with Lanczos
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-        # Comparison with Classical
-        @test Jchol[1:500,1:500] ≈ Jclass[1:500,1:500]
-    end
-
-    @testset "w(x) = (1-x^2)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        wf(x) = (1-x^2)
-        # compute Jacobi matrix via cholesky
-        W = P \ (wf.(x) .* P)
-        Jchol = cholesky_jacobimatrix(Symmetric(W))
-        # compute Jacobi matrix via Lanczos
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison with Lanczos
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-    end
-
-    @testset "w(x) = (1-x^4)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        wf(x) = (1-x^4)
-        # compute Jacobi matrix via cholesky
-        W = P \ (wf.(x) .* P)
-        Jchol = cholesky_jacobimatrix(Symmetric(W))
-        # compute Jacobi matrix via Lanczos
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison with Lanczos
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-    end
-
-    @testset "w(x) = (1.014-x^3)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        wf(x) = 1.014-x^4
-        # compute Jacobi matrix via cholesky
-        W = P \ (wf.(x) .* P)
-        Jchol = cholesky_jacobimatrix(Symmetric(W))
-        # compute Jacobi matrix via Lanczos
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison with Lanczos
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-    end
-end
-
-@testset "Comparison with Lanczos and Classical, with Clenshaw, exponential weights" begin
-    @testset "w(x) = exp(x)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        wf(x) = exp(x)
-        # compute Jacobi matrix via cholesky
-        W = P \ (wf.(x) .* P)
-        Jchol = cholesky_jacobimatrix(Symmetric(W))
-        # compute Jacobi matrix via Lanczos
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison with Lanczos
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-    end
-    
-    @testset "w(x) = (1-x)*exp(x)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        wf(x) = (1-x)*exp(x)
-        # compute Jacobi matrix via cholesky
-        W = P \ (wf.(x) .* P)
-        Jchol = cholesky_jacobimatrix(Symmetric(W))
-        # compute Jacobi matrix via Lanczos
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison with Lanczos
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-    end
-    
-    @testset "w(x) = (1-x^2)*exp(x^2)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        wf(x) = (1-x^2)*exp(x^2)
-        # compute Jacobi matrix via cholesky
-        W = P \ (wf.(x) .* P)
-        Jchol = cholesky_jacobimatrix(Symmetric(W))
-        # compute Jacobi matrix via Lanczos
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison with Lanczos
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
-    end
-    
-    @testset "w(x) = x*(1-x^2)*exp(-x^2)" begin
-        P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
-        x = axes(P,1)
-        J = jacobimatrix(P)
-        Jx = symmjacobim(J)
-        wf(x) = x*(1-x^2)*exp(-x^2)
-        # compute Jacobi matrix via cholesky
-        W = P \ (wf.(x) .* P)
-        Jchol = cholesky_jacobimatrix(Symmetric(W))
-        # compute Jacobi matrix via Lanczos
-        Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
-        # Comparison with Lanczos
-        @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+    @testset "Using Clenshaw with exponential weights" begin
+        @testset "w(x) = exp(x)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            wf(x) = exp(x)
+            # compute Jacobi matrix via cholesky
+            W = P \ (wf.(x) .* P)
+            Jchol = cholesky_jacobimatrix(Symmetric(W))
+            # compute Jacobi matrix via Lanczos
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison with Lanczos
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
+        
+        @testset "w(x) = (1-x)*exp(x)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            wf(x) = (1-x)*exp(x)
+            # compute Jacobi matrix via cholesky
+            W = P \ (wf.(x) .* P)
+            Jchol = cholesky_jacobimatrix(Symmetric(W))
+            # compute Jacobi matrix via Lanczos
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison with Lanczos
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
+        
+        @testset "w(x) = (1-x^2)*exp(x^2)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            wf(x) = (1-x^2)*exp(x^2)
+            # compute Jacobi matrix via cholesky
+            W = P \ (wf.(x) .* P)
+            Jchol = cholesky_jacobimatrix(Symmetric(W))
+            # compute Jacobi matrix via Lanczos
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison with Lanczos
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
+        
+        @testset "w(x) = x*(1-x^2)*exp(-x^2)" begin
+            P = Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])
+            x = axes(P,1)
+            J = jacobimatrix(P)
+            wf(x) = x*(1-x^2)*exp(-x^2)
+            # compute Jacobi matrix via cholesky
+            W = P \ (wf.(x) .* P)
+            Jchol = cholesky_jacobimatrix(Symmetric(W))
+            # compute Jacobi matrix via Lanczos
+            Jlanc = jacobimatrix(LanczosPolynomial(@.(wf.(x)),Normalized(Legendre()[affine(0..1,Inclusion(-1..1)),:])))
+            # Comparison with Lanczos
+            @test Jchol[1:500,1:500] ≈ Jlanc[1:500,1:500]
+        end
     end
 end
