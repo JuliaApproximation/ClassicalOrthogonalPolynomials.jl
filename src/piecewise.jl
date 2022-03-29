d31e2d15282526513cedd3937590370d024ebd73
Sheehan Olver <solver@mac.com>
2022-03-29 20:56:33 +0100

    Work on conversion

--- src/piecewise.jl
+++ src/piecewise.jl
@@ -3,6 +3,11 @@ struct PiecewisePolynomial{T,Bas,P<:AbstractVector} <: Basis{T}
     points::P
 end
 
+PiecewisePolynomial{T}(basis::AbstractQuasiMatrix, points::AbstractVector) where T =
+    PiecewisePolynomial{T,typeof(basis),typeof(points)}(basis, points)
+PiecewisePolynomial(basis::AbstractQuasiMatrix{T}, points::AbstractVector) where T =
+    PiecewisePolynomial{T}(basis, points)
+
 struct ContinuousPolynomial{order,T,P<:AbstractVector} <: Basis{T}
     points::P
 end
@@ -17,13 +22,16 @@ axes(B::PiecewisePolynomial) =
 axes(B::ContinuousPolynomial{1}) where o =
     (Inclusion(first(B.points)..last(B.points)), blockedrange(Vcat(length(B.points), Fill(length(B.points)-1, ∞)))) 
 
+==(::PiecewisePolynomial, ::ContinuousPolynomial) = false
+==(::ContinuousPolynomial, ::PiecewisePolynomial) = false
+
 function getindex(P::PiecewisePolynomial{T}, x::Number, Kk::BlockIndex{1}) where T
     K,k = block(Kk),blockindex(Kk)
     b = searchsortedlast(P.points,x)
-    if b == length(P.points)
-        P.basis[affine(P.points[b-1]..P.points[b],axes(P.basis,1))[x],Int(K)-1]
+    if b == length(P.points) == k + 1 # last point
+        P.basis[affine(P.points[end-1]..P.points[end],axes(P.basis,1))[x],Int(K)]
     elseif b == k
-        P.basis[affine(P.points[b]..P.points[b+1],axes(P.basis,1))[x],Int(K)-1]
+        P.basis[affine(P.points[b]..P.points[b+1],axes(P.basis,1))[x],Int(K)]
     else
         zero(T)
     end
@@ -35,9 +43,7 @@ function getindex(P::ContinuousPolynomial{1,T}, x::Number, Kk::BlockIndex{1}) wh
         LinearSpline(P.points)[x,k]
     else
         b = searchsortedlast(P.points,x)
-        if b == length(P.points)
-            Weighted(Jacobi{T}(1,1))[affine(P.points[b-1]..P.points[b],ChebyshevInterval{real(T)}())[x],Int(K)-1]
-        elseif b == k
+        if b == k
             Weighted(Jacobi{T}(1,1))[affine(P.points[b]..P.points[b+1],ChebyshevInterval{real(T)}())[x],Int(K)-1]
         else
             zero(T)
@@ -45,21 +51,6 @@ function getindex(P::ContinuousPolynomial{1,T}, x::Number, Kk::BlockIndex{1}) wh
     end
 end
 
-function getindex(P::ContinuousPolynomial{1,T}, x::Number, Kk::BlockIndex{1}) where T
-    K,k = block(Kk),blockindex(Kk)
-    if K == Block(1)
-        LinearSpline(P.points)[x,k]
-    else
-        b = searchsortedlast(P.points,x)
-        if b == length(P.points)
-            Weighted(jacobi(1,1,P.points[b-1]..P.points[b]))[x,Int(K)-1]
-        elseif b == k
-            Weighted(jacobi(1,1,P.points[b]..P.points[b+1]))[x,Int(K)-1]
-        else
-            zero(T)
-        end
-    end
-end
 
 getindex(P::Union{PiecewisePolynomial,ContinuousPolynomial}, x::Number, k::Int) = P[x,findblockindex(axes(P,2),k)]
 
@@ -75,4 +66,24 @@ function grid(V::SubQuasiArray{T,2,<:PiecewisePolynomial,<:Tuple{Inclusion,Block
         ret[:,j] = affine(axes(P.basis,1),pts[j]..pts[j+1])[g]
     end
     ret
+end
+
+function grid(V::SubQuasiArray{T,2,<:ContinuousPolynomial{1},<:Tuple{Inclusion,BlockSlice}}) where T
+    P = parent(V)
+    _,JR = parentindices(Q)
+    pts = P.points
+    grid(view(PiecewisePolynomial(Weighted(Jacobi{T}(1,1)), pts),:,JR))
+end
+
+#######
+# Conversion
+#######
+
+function \(P::PiecewisePolynomial{T,<:Legendre}, C::ContinuousPolynomial{1,V}) where {T,V}
+    @assert P.points == C.points
+
+    v = mortar(Fill.((2:2:∞) ./ (3:2:∞), length(P.points)))
+
+    # L = Legendre{T}() \ Weighted(Jacobi{T}(1,1))
+
 end
--- test/test_piecewise.jl
+++ test/test_piecewise.jl
@@ -1,8 +1,18 @@
 using ClassicalOrthogonalPolynomials, Test
-import ClassicalOrthogonalPolynomials: PiecewisePolynomial
+import ClassicalOrthogonalPolynomials: ContinuousPolynomial, PiecewisePolynomial
 
-P = PiecewisePolynomial{1}(range(0,1;length=4))
+
+P = PiecewisePolynomial(Legendre(), range(0,1;length=4))
+C = ContinuousPolynomial{1}(range(0,1;length=4))
+
+P \ C
+
+plot(C[:,1:4])
 
 xx = range(0,1;length=100)
-P[xx,1:10]
-P[1,1:10]
+plot(xx,P[xx,1:6])
+plot(xx,C[xx,1:6])
+C[1,1:10]
+
+x = axes(C,1)
+C \ x
