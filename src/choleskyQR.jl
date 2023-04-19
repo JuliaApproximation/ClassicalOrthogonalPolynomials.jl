6693844c7b8a31e28af9b2495c6b8e723c9803af
Timon Salar Gutleb <timon.gutleb@gmail.com>
2023-04-19 06:31:05 +0100

    minor type adjustments

--- src/choleskyQR.jl
+++ src/choleskyQR.jl
@@ -22,8 +22,8 @@ end
 # The generated Jacobi operators are symmetric tridiagonal, so we store their data in cached bands
 mutable struct CholeskyJacobiBands{dv,T} <: AbstractCachedVector{T}
     data::Vector{T}       # store band entries, :dv for diagonal, :ev for off-diagonal
-    U::UpperTriangular    # store upper triangular conversion matrix (needed to extend available entries)
-    UX::AbstractMatrix    # store U*X, where X is the Jacobi matrix of the original P (needed to extend available entries)
+    U::UpperTriangular{T} # store upper triangular conversion matrix (needed to extend available entries)
+    UX::ApplyArray{T}     # store U*X, where X is the Jacobi matrix of the original P (needed to extend available entries)
     datasize::Int         # size of so-far computed block 
 end
 
@@ -31,7 +31,7 @@ end
 function CholeskyJacobiBands{:dv}(W, P::OrthogonalPolynomial{T}) where T
     U = cholesky(W).U
     X = jacobimatrix(P)
-    UX = U*X
+    UX = ApplyArray(*,U,X)
     dv = zeros(T,2) # compute a length 2 vector on first go
     dv[1] = dot(view(UX,1,1), U[1,1] \ [one(T)])
     dv[2] = dot(view(UX,2,1:2), U[1:2,1:2] \ [zero(T); one(T)])
@@ -40,7 +40,7 @@ end
 function CholeskyJacobiBands{:ev}(W, P::OrthogonalPolynomial{T}) where T
     U = cholesky(W).U
     X = jacobimatrix(P)
-    UX = U*X
+    UX = ApplyArray(*,U,X)
     ev = zeros(T,2) # compute a length 2 vector on first go
     ev[1] = dot(view(UX,1,1:2), U[1:2,1:2] \ [zero(T); one(T)])
     ev[2] = dot(view(UX,2,1:3), U[1:3,1:3] \ [zeros(T,2); one(T)])
@@ -67,8 +67,9 @@ function cache_filldata!(J::CholeskyJacobiBands{:dv,T}, inds::UnitRange{Int}) wh
     getindex(J.U,inds[end]+1,inds[end]+1)
     getindex(J.UX,inds[end]+1,inds[end]+1)
 
+    ek = [zero(T); one(T)]
     @inbounds for k in inds
-        J.data[k] = dot(view(J.UX,k,k-1:k), J.U[k-1:k,k-1:k] \ [zero(T); one(T)])
+        J.data[k] = dot(view(J.UX,k,k-1:k), J.U[k-1:k,k-1:k] \ ek)
     end
 end
 function cache_filldata!(J::CholeskyJacobiBands{:ev, T}, inds::UnitRange{Int}) where T
@@ -76,8 +77,9 @@ function cache_filldata!(J::CholeskyJacobiBands{:ev, T}, inds::UnitRange{Int}) w
     getindex(J.U,inds[end]+1,inds[end]+1)
     getindex(J.UX,inds[end]+1,inds[end]+1)
 
+    ek = [zeros(T,2); one(T)]
     @inbounds for k in inds
-        J.data[k] = dot(view(J.UX,k,k-1:k+1), J.U[k-1:k+1,k-1:k+1] \ [zeros(T,2); one(T)])
+        J.data[k] = dot(view(J.UX,k,k-1:k+1), J.U[k-1:k+1,k-1:k+1] \ ek)
     end
 end
 
@@ -116,8 +118,8 @@ end
 # The generated Jacobi operators are symmetric tridiagonal, so we store their data in cached bands
 mutable struct QRJacobiBands{dv,T} <: AbstractCachedVector{T}
     data::Vector{T}       # store band entries, :dv for diagonal, :ev for off-diagonal
-    U::UpperTriangular    # store upper triangular conversion matrix (needed to extend available entries)
-    UX::AbstractMatrix    # store U*X, where X is the Jacobi matrix of the original P (needed to extend available entries)
+    U::ApplyArray{T}      # store upper triangular conversion matrix (needed to extend available entries)
+    UX::ApplyArray{T}     # store U*X, where X is the Jacobi matrix of the original P (needed to extend available entries)
     datasize::Int         # size of so-far computed block 
 end
 
@@ -126,21 +128,21 @@ function QRJacobiBands{:dv}(sqrtW, P::OrthogonalPolynomial{T}) where T
     U = qr(sqrtW).R
     U = ApplyArray(*,Diagonal(sign.(view(U,band(0)))),U)
     X = jacobimatrix(P)
-    UX = U*X
+    UX = ApplyArray(*,U,X)
     dv = zeros(T,2) # compute a length 2 vector on first go
     dv[1] = dot(view(UX,1,1), U[1,1] \ [one(T)])
     dv[2] = dot(view(UX,2,1:2), U[1:2,1:2] \ [zero(T); one(T)])
-    return QRJacobiBands{:dv,T}(dv, UpperTriangular(U), UX, 2)
+    return QRJacobiBands{:dv,T}(dv, U, UX, 2)
 end
 function QRJacobiBands{:ev}(sqrtW, P::OrthogonalPolynomial{T}) where T
     U = qr(sqrtW).R
     U = ApplyArray(*,Diagonal(sign.(view(U,band(0)))),U)
     X = jacobimatrix(P)
-    UX = U*X
+    UX = ApplyArray(*,U,X)
     ev = zeros(T,2) # compute a length 2 vector on first go
     ev[1] = dot(view(UX,1,1:2), U[1:2,1:2] \ [zero(T); one(T)])
     ev[2] = dot(view(UX,2,1:3), U[1:3,1:3] \ [zeros(T,2); one(T)])
-    return QRJacobiBands{:ev,T}(ev, UpperTriangular(U), UX, 2)
+    return QRJacobiBands{:ev,T}(ev, U, UX, 2)
 end
 
 size(::QRJacobiBands) = (ℵ₀,) # Stored as an infinite cached vector
@@ -163,8 +165,9 @@ function cache_filldata!(J::QRJacobiBands{:dv,T}, inds::UnitRange{Int}) where T
     getindex(J.U,inds[end]+1,inds[end]+1)
     getindex(J.UX,inds[end]+1,inds[end]+1)
 
+    ek = [zero(T); one(T)]
     @inbounds for k in inds
-        J.data[k] = dot(view(J.UX,k,k-1:k), J.U[k-1:k,k-1:k] \ [zero(T); one(T)])
+        J.data[k] = dot(view(J.UX,k,k-1:k), J.U[k-1:k,k-1:k] \ ek)
     end
 end
 function cache_filldata!(J::QRJacobiBands{:ev, T}, inds::UnitRange{Int}) where T
@@ -172,8 +175,9 @@ function cache_filldata!(J::QRJacobiBands{:ev, T}, inds::UnitRange{Int}) where T
     getindex(J.U,inds[end]+1,inds[end]+1)
     getindex(J.UX,inds[end]+1,inds[end]+1)
 
+    ek = [zeros(T,2); one(T)]
     @inbounds for k in inds
-        J.data[k] = dot(view(J.UX,k,k-1:k+1), J.U[k-1:k+1,k-1:k+1] \ [zeros(T,2); one(T)])
+        J.data[k] = dot(view(J.UX,k,k-1:k+1), J.U[k-1:k+1,k-1:k+1] \ ek)
     end
 end
 
