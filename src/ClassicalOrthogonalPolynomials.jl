5311cadb25b4d8891f295b8acec45461af624a28
Sheehan Olver <solver@mac.com>
2024-10-21 19:36:34 +0100

    Update ClassicalOrthogonalPolynomials.jl

--- src/ClassicalOrthogonalPolynomials.jl
+++ src/ClassicalOrthogonalPolynomials.jl
@@ -211,12 +211,12 @@ singularitiesview(w, ::Inclusion) = w # for now just assume it doesn't change
 singularitiesview(w, ind) = view(w, ind)
 singularities(S::SubQuasiArray) = singularitiesview(singularities(parent(S)), parentindices(S)[1])
 
+struct NoSingularities end
 
 basis_singularities(ax, ::NoSingularities) = basis(ax)
 basis_singularities(ax, sing) = basis_singularities(sing)
 basis_axes(ax::Inclusion{<:Any,<:AbstractInterval}, v) = convert(AbstractQuasiMatrix{eltype(v)}, basis_singularities(ax, singularities(v)))
 
-struct NoSingularities end
 
 singularities(::Number) = NoSingularities()
 singularities(r::Base.RefValue) = r[] # pass through
