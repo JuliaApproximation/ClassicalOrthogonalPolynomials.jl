8cdb3e386b33e273eb1b782ecfb23eec95769205
Sheehan Olver <solver@mac.com>
2024-12-07 12:58:33 +0000

    Update test_dynamicpolynomials.jl

--- test/test_dynamicpolynomials.jl
+++ test/test_dynamicpolynomials.jl
@@ -9,4 +9,5 @@ using DynamicPolynomials, ClassicalOrthogonalPolynomials, Test
     @test chebyshevu(5,x) == ultrasphericalc(5,1,x) == 6x - 32x^3 + 32x^5
     @test legendrep(5,x) ≈ (15x - 70x^3 + 63x^5)/8
     @test hermiteh(5,x) == 120x - 160x^3 + 32x^5
+    jacobip(5,1,0,x)
 end
