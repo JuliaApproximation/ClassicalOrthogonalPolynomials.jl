ccbe9aaaa0f6f801e7d4772bf224823572c92396
Sheehan Olver <solver@mac.com>
2025-10-21 16:43:56 +0100

    Update clenshaw.jl

--- src/clenshaw.jl
+++ src/clenshaw.jl
@@ -52,8 +52,6 @@ function copyto!(dest::AbstractVector, V::SubArray{<:Any,1,<:OrthogonalPolynomia
 end
 
 _getindex(::Type{Tuple{IND1,IND2}}, P::OrthogonalPolynomial, (x,n)::Tuple{IND1,AbstractVector{IND2}}) where {IND1,IND2} = layout_getindex(P, x, n)
-_getindex(::Type{Tuple{IND1,IND2}}, P::OrthogonalPolynomial, (x,n)::Tuple{AbstractVector{IND1},AbstractVector{IND2}}) where {IND1,IND2} = layout_getindex(P, x, n)
-_getindex(::Type{Tuple{IND1}}, P::SubArray{<:Any,1,<:OrthogonalPolynomial}, x::AbstractVector{IND1}) where {IND1} = layout_getindex(P, x)
 Base.@propagate_inbounds function _getindex(::Type{IND}, P::OrthogonalPolynomial, (x,n)::IND) where IND
     @boundscheck checkbounds(P, x, n)
     Base.unsafe_getindex(P, x, n)
