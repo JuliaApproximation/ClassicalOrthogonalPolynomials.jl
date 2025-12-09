881d242310f7b2946f9de8f93be76b98b3e1f96b
Sheehan Olver <solver@mac.com>
2025-12-09 11:21:49 +0000

    Update taumethod.jl

--- examples/taumethod.jl
+++ examples/taumethod.jl
@@ -3,18 +3,24 @@ using ClassicalOrthogonalPolynomials
 T = ChebyshevT()
 C⁴ = Ultraspherical(4)
 
-n = 10 # truncation size
+n = 20 # truncation size
+R  = (C⁴\T)[1:n,1:n] # discretization of I
 D¹ = (C⁴\diff(T))[1:n,1:n]
 D⁴ = (C⁴\diff(T,4))[1:n,1:n]
 γ₀ = T[[begin,end],1:n]
 γ₁ = diff(T)[[begin,end],1:n]
 
-z = zeros(2,n)
+φ₁ = (C⁴\T)[1:n,n-1]
+φ₂ = (C⁴\T)[1:n,n]
 
-A = [γ₀     z       z;
-     z      γ₀      z;
-     γ₁     z       z;
-     z      γ₁      z;
-     I      -D¹     0I;
-     D⁴     0I      I;
-     0I     D⁴      D¹]
+Z = zeros(2,n)
+𝐳₂ = zeros(2)
+𝐳 = zeros(n)
+
+A = [γ₀     Z       Z       𝐳₂   𝐳₂;
+     Z      γ₀      Z       𝐳₂   𝐳₂;
+     γ₁     Z       Z       𝐳₂   𝐳₂;
+     Z      γ₁      Z       𝐳₂   𝐳₂;
+     R      -D¹     0I      𝐳   𝐳;
+     D⁴     0I      R       𝐳   𝐳;
+     0I     D⁴      D¹      φ₁  φ₂]
