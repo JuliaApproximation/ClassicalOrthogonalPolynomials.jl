e9a7289f2770916f4a4a72e480c1abe76cd976fb
Sheehan Olver <solver@mac.com>
2025-12-15 21:11:27 +0000

    non symmetric example

--- examples/nonlinearode.jl
+++ examples/nonlinearode.jl
@@ -1,7 +1,7 @@
 ####
 # solve
 #
-# -u'' + u^2 = 1
+# -u'' + u^2 = 1 + x
 # u(±1) = 0
 # 
 # using integrated Legendre w/ Newton iteration.
@@ -9,19 +9,19 @@
 
 using ClassicalOrthogonalPolynomials, Plots
 
-P = Legendre()
-C = Ultraspherical(3/2)
-W = Weighted(C)
-Δ = -diff(W)'diff(W)
+P = Legendre(); C = Ultraspherical(3/2); W = Weighted(C)
+x = axes(P,1)
+Δ = diff(W)'diff(W)
 M = W'W
-b = W'one(axes(W,1))
+b = W'*(1 .+ x)
 F = u -> Δ*u + M * (W\((W*u) .^ 2)) - b
-J = u -> Δ + M * (W \ ((P * (P\(W*u))) .* W))
+J = u -> Δ + 2*M * (W \ ((P * (P\(W*u))) .* W))
 
 
-u = b
+u = zeros(∞)
 while norm(F(u)) ≥ 1E-12
     u -= J(u) \ F(u)
 end
+
 
 plot(W*u)
